049e712c2c93fb85ee67a56b69ec652e663a2a9c
roye <1755431269@qq.com>
2015-11-23 09:49:27 +0800

    win.js

---
+++ js/win.js
@@ -0,0 +1,38 @@
+#win{
+
+    border:1px red solid;
+    width:300px;
+    height: 200px;
+    position:absolute;
+    /*设置左边界和右边界*/
+    top:100px;
+    left:350px;
+    /*让窗口一开始时不可见的*/
+    display:none;
+
+
+}
+/*控制标题栏的样式**/
+#title{
+
+    /*控制标题栏的背景色**/
+    background-color: aqua;
+    /*控制标题栏的左内边距*/
+    padding-left: 3px;
+
+}
+
+/*控制内容去的样式*/
+#content{
+
+    padding-left: 3px;
+    padding-top: 5px;
+}
+/*写一个关闭的按钮*/
+#close{
+
+    /*关闭按钮向右移动**/
+    margin-left: 225px;
+    /*鼠标在X的上方是有个小手*/
+    cursor:pointer;
+}
